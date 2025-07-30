486c4c38206abf07bdac395b0e7de5b43b09a2ca
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2025-07-30 15:28:41 -0400

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",
+  base: "/fakeha-38.github.io/",
   plugins: [react()],
 })
 
